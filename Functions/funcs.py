c304f93a0eb83aac78fa320b475c58095dea56f7
bsmith001 <smith.bradk@outlook.com>
2022-03-08 19:48:14 +0000

    blah

--- Functions/funcs.py
+++ Functions/funcs.py
@@ -1,4 +1,14 @@
-def my_function():
+
+def my_function(arg1, arg2, arg3, arg4):
+    print(f"Tacocat gas two new friends! meet {arg1} and {arg2}!")
+
+
+def main():
     print("tacocat!")
 
-my_function()
+if __name__ == "__main__":
+    print(f"the value of __name__ is: {__name__}")
+    my_function(arg1="bob", arg2="sue", arg3="onion", arg4="potato")
+
+
+
