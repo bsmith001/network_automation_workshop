e4bc85cd8862e862c04ef518a43c37f447026abb
bsmith001 <smith.bradk@outlook.com>
2022-03-08 20:43:24 +0000

    blah1

--- Functions/funcs.py
+++ Functions/funcs.py
@@ -1,14 +1,18 @@
 
-def my_function(arg1, arg2, arg3, arg4):
-    print(f"Tacocat gas two new friends! meet {arg1} and {arg2}!")
 
-
-def main():
-    print("tacocat!")
+def connect(hostname,*, port, username="racecar", password):
+    if hostname.count(".") == 3:
+        print(f"Connecting to device {hostname}:{port}, as user {username}!")
+        return True
+    else:
+        print(f"Invalid IP provided, cannot continue ...")
+        return False
 
 if __name__ == "__main__":
-    print(f"the value of __name__ is: {__name__}")
-    my_function(arg1="bob", arg2="sue", arg3="onion", arg4="potato")
+    result = connect(hostname="1.1.1.1", port="22", username="tacocat", password="racecare")
+    print(f"result1: {result}")
+    result = connect(hostname="2.2.2.2", port="22", password="potato")
+    print(f"result2: {result}")
 
 
 
