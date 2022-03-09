d928783d95dbbd4829a6b10311697edf60bd84e1
bsmith001 <smith.bradk@outlook.com>
2022-03-09 23:14:44 +0000

    hah

--- http_api.py
+++ http_api.py
@@ -1,10 +1,26 @@
+import json
 
+import httpx
 
 
 def main():
+    nxapi_payload = """
+{
+   "ins_api": {
+     "version": "1.0",
+     "type": "cli_show",
+     "chunk": "0",
+     "sid": "1",
+     "input": "show version",
+     "output_format": "json"
+  }
+}"""
 
+    nxapi_payload_dict = json.loads(nxapi_payload)
+    print(nxapi_payload_dict)
 
-
+    reponse = httpx.post("https://leaf3/ins", json=nxapi_payload_dict, auth=("admin", "admin"), verify=False)
+    print(reponse)
 
 
 
