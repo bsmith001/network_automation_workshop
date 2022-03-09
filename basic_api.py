674ebd937aeb43be286a591af6495dd9aa78d2bc
bsmith001 <smith.bradk@outlook.com>
2022-03-09 22:16:01 +0000

    blah

--- basic_api.py
+++ basic_api.py
@@ -1,11 +1,19 @@
-
+import httpx
 
 
 def main():
 
+    r = httpx.get('http://brad-netbox.workshop/')
+    print(r, type(r), dir(r))
 
+    print(r.text)
 
+    print(r.headers, r.status_code, r.elapsed)
 
+    r = httpx.get("http://brad-netbox.workshop/api/dcim/devices", headers={"Authorization": "Token a546e15a088bc072150c67d65ed0bf7b3fad803c"})
+    print(f"Fetching dcim/devices response: {r}")
+    print(r.text)
+    print(r.json())
 
 
 if __name__ == "__main__":
