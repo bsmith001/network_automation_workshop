8ec110919a8e6e32784c898fe46fac886be82ae4
bsmith001 <smith.bradk@outlook.com>
2022-03-09 21:51:38 +0000

    blah

--- regex/regex.py
+++ regex/regex.py
@@ -1,10 +1,31 @@
+import re
 
 
+rconf = """
+Building configuration...
+Current configuration : 5878 bytes!! 
+Last configuration change at 16:01:10 UTC Sat Oct 9 2021
+!
+version 16.12
+service timestamps debug datetime msec
+service timestamps log datetime msec
+! Call-home is enabled by Smart-Licensing.
+service call-home
+platform qfp utilization monitor load 80
+platform punt-keepalive disable-kernel-core
+platform console serial
+!
+hostname rtr1
+!
+boot-start-marker
+boot-end-marker
+!
+"""
 
 def main():
-
-
-
+    result = re.search(r"version \d{2}\.\d{2}", rconf)
+    print(result)
+    print(type(result), dir(result))
 
 
 
