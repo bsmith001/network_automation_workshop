341f3bdc61951656b29a5634dc8c8f182bcd657f
bsmith001 <smith.bradk@outlook.com>
2022-03-10 16:23:01 +0000

    blah

--- j2_env.py
+++ j2_env.py
@@ -1,10 +1,12 @@
-
+from jinja2 import Environment, FileSystemLoader
 
 
 def main():
 
-
-
+    j2_env = Environment(loader=FileSystemLoader("/home/brad/network_automation_workshop/templates"))
+ #   template = j2_env.get_template("interface.j2")
+ #   print(template.render())
+    print(dir(j2_env))
 
 
 
