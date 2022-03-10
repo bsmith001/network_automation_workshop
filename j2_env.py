85aa7cca176f28b6ae7f193b7fd6740dc3d0ac85
bsmith001 <smith.bradk@outlook.com>
2022-03-10 16:57:56 +0000

    lame

--- j2_env.py
+++ j2_env.py
@@ -18,5 +18,20 @@ def main():
     inline_template = Template("Hai fren, I'm {{ person }}, how you doin!?")
     print(inline_template.render(person="tacocat"))
 
+    interfaces = [
+        {"intf_num": "1", "ip_addr": "1.1.1.1/30", "descr": "tacocat was here again!"},
+        {"intf_num": "2", "ip_addr": "2.2.2.2/30", "descr": "racecar potato"},
+        {"intf_num": "3", "ip_addr": "3.3.3.3/30", "descr": "is it lunch yet??"},
+        {"intf_num": "4", "ip_addr": "4.4.4.4/30", "descr": "zzzzzzzzzz"},
+    ]
+
+    interface_configs = []
+    for interface in interfaces:
+        interface_configs.append(template.render(**interface))
+ #       print(template.render(**interface)) 
+
+    print("!\n".join(interface_configs))
+
+
 if __name__ == "__main__":
     main()
